02f91420ac920e980bd27c4b87c94929928bfccc
your.user.name <jeja2306@student.miun.se>
2025-10-13 12:23:59 +0200

    comments

--- Services/SleepPredictionService.cs
+++ Services/SleepPredictionService.cs
@@ -5,30 +5,30 @@ using SleepApp.Models;
 
 namespace SleepApp.Services
 {
-    public static class SleepPredictionService
+    public static class SleepPredictionService // klass för att göra gissningar
     {
-        private static readonly string ModelPath = "Models/sleepModel.zip";
-        private static PredictionEngine<PersonData, SleepPrediction>? predEngine;
+        private static readonly string ModelPath = "Models/sleepModel.zip"; // där modellen är sparad
+        private static PredictionEngine<PersonData, SleepPrediction>? predEngine; // objekt för att göra gissning
 
-        static SleepPredictionService()
+        static SleepPredictionService() // körs när klassen används för första gången
         {
-            var mlContext = new MLContext();
+            var mlContext = new MLContext(); // ingång till ml.net
             DataViewSchema modelSchema;
 
-            var model = mlContext.Model.Load(ModelPath, out modelSchema);
-            predEngine = mlContext.Model.CreatePredictionEngine<PersonData, SleepPrediction>(model);
+            var model = mlContext.Model.Load(ModelPath, out modelSchema); // laddar tränad modell med dess kolumner
+            predEngine = mlContext.Model.CreatePredictionEngine<PersonData, SleepPrediction>(model); // skapar objekt som kan ta in persondata och returnera gissning
         }
 
-        public static string Predict(PersonData input)
+        public static string Predict(PersonData input) // metod för att göra gissningen
         {
             var prediction = predEngine?.Predict(input);
             return prediction?.PredictedLabel ?? "Unknown";
         }
     }
 
-    public class SleepPrediction
+    public class SleepPrediction // output från gissning
     {
-        [ColumnName("PredictedLabel")]
+        [ColumnName("PredictedLabel")] // sorterar kolumn
         public string PredictedLabel { get; set; } = string.Empty;
     }
 }
