d8c172871358b48002c25e8074f1ab0ce1ec7871
your.user.name <jeja2306@student.miun.se>
2025-10-13 11:59:21 +0200

    commit class

---
+++ Models/PersonData.cs
@@ -0,0 +1,22 @@
+using Microsoft.ML.Data;
+
+namespace SleepApp.Models
+{
+    public class Persondata
+    {
+        [LoadColumn(0)]
+        public float SleepHours { get; set; }
+
+        [LoadColumn(0)]
+        public float CaffeineHours { get; set; }
+
+        [LoadColumn(0)]
+        public float StressLevel { get; set; }
+
+        [LoadColumn(0)]
+        public float ActivityLevel { get; set; }
+
+        [LoadColumn(0)]
+        public float SleepQuality { get; set; }
+    }
+}
